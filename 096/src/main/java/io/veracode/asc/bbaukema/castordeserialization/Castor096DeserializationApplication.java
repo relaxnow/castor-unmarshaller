539e5bdfb67b0ce22b0b8e42de544a2e01a5db23
Boy Baukema <bbaukema@veracode.com>
2021-12-06 16:16:32 +0000

    Add example for 0.9.6 with fixed ClassLoader

---
+++ 096/src/main/java/io/veracode/asc/bbaukema/castordeserialization/Castor096DeserializationApplication.java
@@ -0,0 +1,13 @@
+package io.veracode.asc.bbaukema.castordeserialization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Castor096DeserializationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Castor096DeserializationApplication.class, args);
+    }
+
+}
